574c28cee23b6739422e90317054a9f0040c855b
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-24 20:30:18 +0530

    Create appointments.tsx

---
+++ pages/appointments.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Appointments() {
+  return (
+    <div>
+      <h2>Appointments List</h2>
+      {/* Replace with real appointment table */}
+      <p>No appointments yet</p>
+    </div>
+  );
+}
